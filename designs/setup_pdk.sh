e42dc4a0b485b74b753118ff60faf4df3d949ad0
Jianxun Zhu <zhujianxun.bupt@gmail.com>
2025-06-10 20:48:21 -0400

    create simulation directory in `setup_pdk.sh`

--- designs/setup_pdk.sh
+++ designs/setup_pdk.sh
@@ -7,3 +7,5 @@ source iic-pdk-script.sh gf180mcuD
 # Overwrite the xschemrc file to fix the 180MCU_MODELS path TCL variable
 # Overriding .bashrc alias for cp to avoid the -i flag
 \cp -f ./.config/.xschem/xschemrc ~/.xschem/xschemrc
+
+mkdir -p /headless/.xschem/simulations
